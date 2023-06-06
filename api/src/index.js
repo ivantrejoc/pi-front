8ee28d1746046858efe254612b002fedce5176a6
ivantrejoc <IVANJTREJOC@GMAIL.COM>
2023-06-06 15:27:57 -0400

    front levanta

--- api/src/index.js
+++ api/src/index.js
@@ -17,8 +17,8 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+const server = require('./app.js');
+const { conn } = require('./db.js');
 const PORT = 3001;
 
 // Syncing all the models at once.
