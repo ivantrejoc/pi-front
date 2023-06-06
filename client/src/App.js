e838aa148a5e16b6a9298c88876741c69736e654
ivantrejoc <IVANJTREJOC@GMAIL.COM>
2023-06-06 15:29:17 -0400

    front levanta

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <h1>Henry Pokemon</h1>
+      <h1>POKEMON</h1>
     </div>
   );
 }
 
+
+//VIEWS
+//* Landing page
+//* Home renderiza una serie de personajes
+//* Detail: cuando se hace click sobre un personaje, muestra los detalles de este personaje
+//* Form que crea un nuevo personaje
 export default App;
